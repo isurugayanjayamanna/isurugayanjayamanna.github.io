dd567fc47c5252be78a0f3a575465191beca4a99
isurugayanjayamanna <93472577+isurugayanjayamanna@users.noreply.github.com>
2021-11-30 22:41:13 -0800

    update_2
    3.1 agein

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var cnt=document.getElementById("count");
 var water=document.getElementById("water");
 var percent=cnt.innerText;
 var interval;
-var DatabaseData;
+var DatabaseData =cnt.innerText;
 var mock;
 
 (function(){
@@ -15,7 +15,7 @@ var mock;
         storageBucket: "waterlevel-b3c60.appspot.com",
         messagingSenderId: "303485112022",
         appId: "1:303485112022:web:76b2fabce454c046464377"
-  };
+      };
       firebase.initializeApp(config);
       
       //Get elements
@@ -24,33 +24,37 @@ var mock;
       // Create reference
     //   const dbRefObject = firebase.database().ref().child('Time');
 
-            var ref = firebase.database().ref().child('WaterLevelData/Precentage');
+            var ref = firebase.database().ref("WaterLevelData/Precentage/");
 
                 ref.on("value", function(snapshot) {
                     snapshot.forEach(function(childSnapshot) {
-                        var childData = childSnapshot.val();
+                        var childData1 = childSnapshot.val();
                         // var id=childData.id;
-                        console.log(childData);
+                        console.log(childData1);
 
-                        percent = childData;
-                        cnt.innerHTML = percent;
+                        percent = childData1;
+                        // cnt.innerHTML = percent;
+
                         water.style.transform='translate(0'+','+(100-percent)+'%)';
 
                         interval=setInterval(function(){ 
                             //   percent++; 
                             //   cnt.innerHTML = percent; 
 
-                            
+                        
                             //   water.style.transform='translate(0'+','+(100-percent)+'%)';
                             if(percent==100){
                                 clearInterval(interval);
                             }
                         },
                         100);
+                        
+                
                     });
+                
                 });
 
-                var ref = firebase.database().ref().child('WaterLevelData/Water_Level');
+                var ref = firebase.database().ref("WaterLevelData/Water_Level/");
 
                 ref.on("value", function(snapshot) {
                     snapshot.forEach(function(childSnapshot) {
@@ -60,11 +64,14 @@ var mock;
 
                         // percent = childData2;
                         cnt.innerHTML = childData2;
-                        water.style.transform='translate(0'+','+(100-percent)+'%)';
 
-                        
+                        water.style.transform='translate(0'+','+(100-percent)+'%)';           
+                
                     });
+                
                 });
+
+                
 
         
         
